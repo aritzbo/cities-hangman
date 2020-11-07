943c945b304178d9c21f66e3bb42a33b395be7c1
aritzbo <71656434+aritzbo@users.noreply.github.com>
2020-11-07 15:52:16 +0100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,15 +1,20 @@
-import random
+# HANGMAN
+# The puzzle hides a word from you, which you try to guess letter by letter. If you fail, you'll be “hanged”. If you win, you'll survive
 
+import random
 def menu():
     print("H A N G M A N")
     return input('\nType "play" to play the game, "exit" to quit: ')
 
 def hangman():
-    choosen = list(random.choice(['python', 'java', 'kotlin', 'javascript']))   # A random word from the list is choosen
-    hidden = list('-' * len(choosen))                                           # Replace such word with dashes, in order to start guessing through several attempts
-    already_typed = []
+    # The game randomly chooses a string from the list, in this case city names with rather variable lengths
+    choosen = list(random.choice(["prague", "riga", "barcelona", 
+                                  "munich", "bordeaux", "montreal",
+                                  "bratislava", "antofagasta", "bern"]))
+    hidden = list('-' * len(choosen))          # The hidden string is created using the length of the randomly selected word and replacing each character with dashes
+    already_typed = []                         # An empty list where all the input will be appended to, simplyfying the way to detect duplicates
     attempts = 0
-
+    
     while attempts < 8:
         print(''.join(hidden))
         guess = input("Input a letter: ")
@@ -24,7 +29,7 @@ def hangman():
                                 hidden[i] = guess
                     else:
                         print("That letter doesn't appear in the word")
-                        attempts += 1
+                        attempts += 1          # Each time the user inputs a letter that doesn't appear in the word, the attempt counter will increase by 1
             else:
                 print("Please enter a lowercase English letter")
         else:
